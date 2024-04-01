c81cb160dd1b8f542eb5fe0be77209a1b77ffb66
Vo Quoc Thang <voquocthang2310@gmail.com>
2024-04-01 20:19:09 +0700

    test

--- src/controllers/majors.controllers.js
+++ src/controllers/majors.controllers.js
@@ -1,12 +1,34 @@
-const majorsServices = require('../services/majors.services')
+const { majorMessage } = require('../constants/message')
+const majorsService = require('../services/major.services')
 
-const getAllMajorController = async (req, res) => {
-    const result = await majorsServices.getAllMajor()
+const getAllMajor = async (req, res) => {
+    const result = await majorsService.getAllMajor()
     return res.json({
+        message: majorMessage.GET_ALL_MAJOR_SUCCESS,
+        data: result
+    })
+}
+
+const createMajorController = async (req, res) => {
+    const body = req.body
+    const result = await majorsService.createMajor(body)
+    return res.json({
+        message: 'Thêm chuyên ngành thành công',
+        data: result
+    })
+}
+const deleteMajorController = async (req, res) => {
+    const { id } = req.params
+    console.log('id là : ', id)
+    const result = await majorsService.deleteMajorById(id)
+    return res.json({
+        message: 'Xóa thành công',
         data: result
     })
 }
 
 module.exports = {
-    getAllMajorController
+    getAllMajor,
+    createMajorController,
+    deleteMajorController
 }
